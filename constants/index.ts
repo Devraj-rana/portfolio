0fe87e0209b04c8df89e05d6de692fe2c57f7fa8
unknown <devrajnara@gmail.com>
2025-04-01 20:06:42 +0530

    firstt commit

---
+++ constants/index.ts
@@ -0,0 +1,142 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Linked in",
+    href:"https://www.linkedin.com/in/devraj-rana/",
+    src: "/linkedin.svg",   
+    
+  },
+  {
+    name: "Github",
+    href:"https://github.com/Devraj-rana",
+    src: "/github.svg",
+  },
+  {
+    name: "Instagram", 
+    href:"https://www.instagram.com/devraj.rana.07?igsh=MW5iYnZkaTR1N21vaw==",
+    src: "/instagram.svg",
+  
+  },
+];
+export const Projects = [
+  {
+    title: "COER CONNECT",
+    text: "Social networking website for college students.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "BOOMBAZAR.STORE",
+    text: "E-commerce website.",
+    src: "/SpaceWebsite.jpg",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "THIS IS MY PORTFOLIO WEBSITE",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "Matrix themed Website",
+    text: "Under working.",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
